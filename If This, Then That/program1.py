4009802da51abadb99b818f092d25e082b9f1699
arjun-da-skib <arjungupta@browning.edu>
2026-03-02 14:13:58 +0000

    did sum stuf

--- If This, Then That/program1.py
+++ If This, Then That/program1.py
@@ -1,3 +1,11 @@
 # WAP to check if a single character is a vowel or not.
 
-letter = input("Enter a s
+letter = input("Enter a single character plz mr. jeffery: ") # letter variable
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"] # list of vowels
+isvowel = 0
+for i in range(10): # go through list of variables
+    if letter == vowels[i]:
+        print(letter, "is a vowel")
+        isvowel = 1
+if isvowel == 0:
+    print(letter, "is not a vowel")
